d5b6df21c8699217c5869262918a22b6566df603
Cprice <thrashers231@yahoo.com>
2015-02-04 23:45:31 -0500

    -Added a database and created a table to store all the data in -Created a WPF form to update the database

---
+++ GroceryStore/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroceryStore
+{
+    public partial class Item
+    {
+
+        #region Public properties
+        public int SKU { get; set; }
+        public int QuantityReceived { get; set; }
+        public int CurrentStock { get; set; }
+        public DateTime DateRecieved { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        #endregion
+
+        public Item(string brand, string description, int sku, int quantityReceived, int currentStock, DateTime dateReceived)
+        {
+            Brand = brand;
+            Description = description;
+            SKU = sku;
+            QuantityReceived = quantityReceived;
+            CurrentStock = currentStock;
+            DateRecieved = dateReceived;
+        }
+    }
+}
--- GroceryStore/MainWindow.xaml.cs
+++ GroceryStore/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,131 @@ namespace GroceryStore
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Item> items = new List<Item>();
+        private const int SQL_DUPLICATE_PRIMARY_KEY_ERROR_NUM = 2627;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void InitInventory()
+        {
+            stockDataGrid.ItemsSource = null;
+            items.Clear();
+            stackPanelOverview.Children.Clear();
+            using (SqlConnection conn =
+                    new SqlConnection(
+                        @"Data Source=(LocalDB)\v11.0;AttachDbFilename=" + System.AppDomain.CurrentDomain.BaseDirectory + "Inventory.mdf;Integrated Security=True")
+                )
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "Select * From Stock";
+
+                    try
+                    {
+                        conn.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                items.Add(new Item(reader.GetString(1), reader.GetString(5),
+                                    reader.GetInt32(0), reader.GetInt32(3),
+                                    reader.GetInt32(4), reader.GetDateTime(2)));
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Error connecting to the database!", "Error", MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+
+            stockDataGrid.ItemsSource = items;
+        }
+
+        private void AddItem(int SKU, string description, string brand, int quantityReceived, int currentStock,
+            DateTime dateReceived)
+        {
+            using (var conn =
+                    new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=" + System.AppDomain.CurrentDomain.BaseDirectory + "Inventory.mdf;Integrated Security=True"))
+            {
+                using (var cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText =
+                        "INSERT INTO Stock (SKU, Brand, DateReceived, QuantityReceived, CurrentStock, ItemDescription) VALUES (@SKU, @Brand, @DateReceived, @QuantityReceived, @CurrentStock, @ItemDescription)";
+                    cmd.Parameters.AddWithValue("@SKU", SKU);
+                    cmd.Parameters.AddWithValue("@ItemDescription", description);
+                    cmd.Parameters.AddWithValue("@Brand", brand);
+                    cmd.Parameters.AddWithValue("@QuantityReceived", quantityReceived);
+                    cmd.Parameters.AddWithValue("@CurrentStock", currentStock);
+                    cmd.Parameters.AddWithValue("@DateReceived", dateReceived);
+
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (SqlException e)
+                    {
+                        //TODO: just add to the current stock ??
+                        if (e.Number == SQL_DUPLICATE_PRIMARY_KEY_ERROR_NUM)
+                        {
+                            MessageBox.Show("A Product with this SKU already exists!", "Error", MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error connecting to the database!", "Error", MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+                        }
+                       
+                    }
+                    finally
+                    {
+                        conn.Close(); 
+                    }
+                }
+            }
+        }
+        
+
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            InitInventory();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                AddItem(int.Parse(textBoxSKU.Text), textBoxDescription.Text, textBoxBrand.Text, int.Parse(textBoxQuantityReceived.Text), int.Parse(textBoxQuantityReceived.Text), DateTime.Parse(datePickerReceived.Text));
+            }
+            catch (FormatException)
+            {
+
+                MessageBox.Show("One of the inputs is invalid!", "Error", MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+            }
+            
+        }
+
+
+
+
     }
 }
